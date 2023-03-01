47b68466fb96e4a106b033bef16f4823a356b246
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-02 00:40:31 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./styles/style.scss";
 import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
       <h1>BBQ App</h1>
+      <Footer />
     </div>
   );
 }
