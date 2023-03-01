a6230b971ad5fcd2fd72815e8ab48b4eaec279e2
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-02 00:36:27 +0100

    add font awesome and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./styles/style.scss";
+import Navbar from "./components/shared/Navbar";
+
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>BBQ App</h1>
     </div>
   );
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import mainLogo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <img src={mainLogo} alt="A piece of meat with fork" />
+      </Link>
+      <ul>
+        <li>
+          <Link>
+            <FontAwesomeIcon icon={solid("utensils")} /> Menu
+          </Link>
+        </li>
+        <li>
+          <Link to="">
+            <FontAwesomeIcon icon={solid("phone")} /> Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
