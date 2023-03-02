28a124f3b5349f3f35f15f38dd1b0c2fd3f73718
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-02 01:36:28 +0100

    add responsive styles to categories section

--- src/components/home/CategoriesContainer.jsx
+++ src/components/home/CategoriesContainer.jsx
@@ -21,6 +21,21 @@ export default function CategoriesContainer() {
           </Link>
         </div>
       </article>
+      <article>
+        <img className="category-img" src={beefImg} alt="Beef Background" />
+        <div>
+          <h2>Beef Dishes</h2>
+          <p>
+            In our wonderful dishes, you'll find wholesome vegan meals made with
+            affordable and protein-rich whole grains, beans, and nuts. You'll
+            also see plenty of fresh, colorful produce, and occasionally some
+            tofu. No funny meat substitutes here.
+          </p>
+          <Link to="">
+            <FontAwesomeIcon icon={solid("chevron-right")} /> View menu
+          </Link>
+        </div>
+      </article>
     </div>
   );
 }
