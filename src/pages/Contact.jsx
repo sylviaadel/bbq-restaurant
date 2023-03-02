6b345170268ec952f5911d096aae4f7ea4e9d190
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-02 13:52:47 +0100

    add styles and fix typo

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
-import BookTable from "../components/Contact/BookTable";
-import OpeningTimes from "../components/Contact/OpeningTimes";
-import Address from "../components/Contact/Address";
-import Map from "../components/Contact/Map";
+import BookTable from "../components/contact/BookTable";
+import OpeningTimes from "../components/contact/OpeningTimes";
+import Address from "../components/contact/Address";
+import Map from "../components/contact/Map";
 import ChefImg from "../assets/images/chef-img.png";
 
 export default function Contact() {
