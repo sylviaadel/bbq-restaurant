c6a017591f266a85e0c6ace2854a626249030428
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-03 22:08:16 +0100

    refactor react

--- src/components/admin/AddCategory.jsx
+++ src/components/admin/AddCategory.jsx
@@ -10,34 +10,31 @@ export default function AddCategory() {
     console.log("data", title, description, imageURL);
   }
   return (
-    <section id="add-category">
-      <h2>Add a new Category</h2>
-      <form onSubmit={(e) => onSubmit(e)}>
-        <label>
-          <span>Title</span>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Image URL</span>
-          <input
-            type="text"
-            value={imageURL}
-            onChange={(e) => setImageURL(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>Description</span>
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
-        </label>
-        <button className="primary-btn">Add Category</button>
-      </form>
-    </section>
+    <form onSubmit={(e) => onSubmit(e)}>
+      <label>
+        <span>Title</span>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Image URL</span>
+        <input
+          type="text"
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Description</span>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </label>
+      <button className="primary-btn">Add Category</button>
+    </form>
   );
 }
--- src/components/admin/AddProduct.jsx
+++ src/components/admin/AddProduct.jsx
@@ -1,35 +1,32 @@
 export default function AddProduct() {
   return (
-    <section id="add-product">
-      <h2>Add a new Product</h2>
-      <form>
-        <label>
-          <span>Title</span>
-          <input type="text" />
-        </label>
-        <label>
-          <span>Image URL</span>
-          <input type="text" />
-        </label>
-        <label>
-          <span>Related Category</span>
-          <select>
-            <option>Beef</option>
-            <option>Chicken</option>
-            <option>Beverages</option>
-          </select>
-        </label>
-        <label>
-          <span>Price</span>
-          <input type="text" />
-          <span className="currency">SEK</span>
-        </label>
-        <label>
-          <span>Description</span>
-          <textarea></textarea>
-        </label>
-        <button className="primary-btn">Add Product</button>
-      </form>
-    </section>
+    <form>
+      <label>
+        <span>Title</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Image URL</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Related Category</span>
+        <select>
+          <option>Beef</option>
+          <option>Chicken</option>
+          <option>Beverages</option>
+        </select>
+      </label>
+      <label>
+        <span>Price</span>
+        <input type="text" />
+        <span className="currency">SEK</span>
+      </label>
+      <label>
+        <span>Description</span>
+        <textarea></textarea>
+      </label>
+      <button className="primary-btn">Add Product</button>
+    </form>
   );
 }
