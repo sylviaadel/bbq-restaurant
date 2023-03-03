4a54c77a7fa1b971b12441157aabb5a1dee7278e
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-03 18:32:37 +0100

    add readDocument to firestore

--- src/scripts/fireStore.js
+++ src/scripts/fireStore.js
@@ -1,11 +1,20 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
 import { database } from "./firebaseSetup";
 
+export async function readDocument(collectionName, documentId) {
+  const reference = doc(database, collectionName, documentId);
+  const snapshot = await getDoc(reference);
+  const result = { id: snapshot.id, ...snapshot.data() };
+
+  return result;
+}
+
 export async function readDocuments(collectionName) {
-  const querySnapshot = await getDocs(collection(database, collectionName));
+  const reference = collection(database, collectionName);
+  const snapshot = await getDocs(reference);
   const result = [];
 
-  querySnapshot.forEach((doc) => {
+  snapshot.forEach((doc) => {
     const document = { id: doc.id, ...doc.data() };
     result.push(document);
   });
