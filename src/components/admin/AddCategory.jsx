8fec2505db1cc67bd4585e98218cf47b6fed7dc9
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-03 22:06:33 +0100

    add read single document

--- src/components/admin/AddCategory.jsx
+++ src/components/admin/AddCategory.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 export default function AddCategory() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageURL, setImageURL] = useState("");
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log("data", title, description, imageURL);
+  }
   return (
     <section id="add-category">
       <h2>Add a new Category</h2>
-      <form>
+      <form onSubmit={(e) => onSubmit(e)}>
         <label>
           <span>Title</span>
-          <input type="text" />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </label>
         <label>
           <span>Image URL</span>
-          <input type="text" />
+          <input
+            type="text"
+            value={imageURL}
+            onChange={(e) => setImageURL(e.target.value)}
+          />
         </label>
         <label>
           <span>Description</span>
-          <textarea></textarea>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
         </label>
         <button className="primary-btn">Add Category</button>
       </form>
