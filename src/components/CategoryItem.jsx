f3264c52e129081bccc9adc8993b7bc3c5d45e17
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-04 01:04:07 +0100

    remove unused component and fix styles

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function CategoryItem({ item }) {
   const { id, title, description, imageURL } = item;
 
   return (
     <article key={id}>
-      <img src={imageURL} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <button className="small-btn">View Menu</button>
+      <img className="category-img" src={imageURL} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <Link to="" className="small-btn">
+          View Menu
+        </Link>
+      </div>
     </article>
   );
 }
