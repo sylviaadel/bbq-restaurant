54e40a20fe4d40bc3886fa229df045fe1c0811d7
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-07 23:25:53 +0100

    Add leaflet map

--- src/components/Contact/Map.jsx
+++ src/components/Contact/Map.jsx
@@ -1,9 +1,24 @@
-import MapImg from "../../assets/images/staticmap.png";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LocationIcon } from "../../helpers/LocationIcon";
 
 export default function Map() {
   return (
-    <section className="map">
-      <img src={MapImg} alt="Main Map of Restaurant with marker." />
-    </section>
+    <div id="Map">
+      <MapContainer
+        center={[59.05361, 9.70177]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[59.05361, 9.70177]} icon={LocationIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
