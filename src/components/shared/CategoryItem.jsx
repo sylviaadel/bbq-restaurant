25d7ff85eaf301d706de5c542296ffabfbf82ba6
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-10 10:38:10 +0100

    Add modal with styles

--- src/components/shared/CategoryItem.jsx
+++ src/components/shared/CategoryItem.jsx
@@ -3,18 +3,22 @@ import { deleteDocument } from "../../scripts/fireStore/deleteDocument";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 import { useCategories } from "../../state/CategoriesProvider";
+import Modal from "./Modal";
+import { useState } from "react";
 
 export default function CategoryItem({ item, collectionName }) {
   const { id, title, description, imageURL } = item;
   const { dispatch } = useCategories();
+  const [isOpen, setIsOpen] = useState(false);
 
   async function onDelete(id) {
-    const message = `Are you sure you want to delete ${title} Category?`;
-    const result = window.confirm(message);
-    if (!result) return;
+    setIsOpen(true);
+    // const message = `Are you sure you want to delete ${title} Category?`;
+    // const result = window.confirm(message);
+    // if (!result) return;
 
-    await deleteDocument(collectionName, id);
-    dispatch({ type: "delete", payload: id });
+    // await deleteDocument(collectionName, id);
+    // dispatch({ type: "delete", payload: id });
   }
 
   return (
@@ -33,6 +37,11 @@ export default function CategoryItem({ item, collectionName }) {
           <FontAwesomeIcon icon={solid("trash")} /> Delete category
         </button>
       </div>
+      <Modal
+        open={isOpen}
+        onClose={(e) => setIsOpen(false)}
+        message="Are you sure you want to delete this category?"
+      />
     </article>
   );
 }
